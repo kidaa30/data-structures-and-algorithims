f58e56cacd0ff01196ffabee8c6a6e3edaa40acb
MichaelScript <me@michaelgreen.net>
2015-09-22 15:40:43 -0700

    Functionality done, needs some refactoring

--- data_structures/ArrayLinearList.java
+++ data_structures/ArrayLinearList.java
@@ -1,12 +1,15 @@
 package data_structures;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ArrayLinearList<E> implements LinearListADT<E> {
 	private E obj;
 	// change to private later
 	private E[] storage;
-	private int head,size,maxIndex,midIndex;
+	private int head,size,maxIndex;
+	// For ease of acccess
+	private int midIndex,iteratorIndex;
 
 	public ArrayLinearList(int size){
 		// Unchecked cast
@@ -22,23 +25,30 @@ public class ArrayLinearList<E> implements LinearListADT<E> {
 			System.out.print(element + ", ");
 		}
 		System.out.println("]\n");
+		System.out.println("head is: " + head);
 	};
 
 
 	/* The adding and removing functions are basically the same, just reverses of each other with slight
 	differences. */
 	public boolean addFirst(E obj){
+		if(this.isFull()){
+			return false;
+		};
 		size++;
-		head = head == 0 ? maxIndex : head;
+		head = head == 0 ? maxIndex + 1 : head;
 		storage[--head] = obj;
 		return true;
 	};
 
 	public boolean addLast(E obj){
+		if(this.isFull()){
+			return false;
+		};
 		size++;
 		int rear = head + size;
 		// If we above the max index just take the difference which will wrap us around
-		rear = rear >= maxIndex ? rear - maxIndex : rear;
+		rear = rear > maxIndex ? rear - maxIndex - 1 : rear;
 		storage[rear] = obj;
 		return true;
 	};   
@@ -46,16 +56,29 @@ public class ArrayLinearList<E> implements LinearListADT<E> {
 	public E removeFirst(){
 		if(this.isEmpty())
 			return null;
-		head = head == maxIndex ? -1 : head;
+		E oldElement = storage[head];
+		head = head == maxIndex ? 0 : head++;
 		size--;
-		return storage[head++];
+		return oldElement;
 	};   
 
 	public E removeLast(){
-		return this.isEmpty() ? null : storage[1 + head + size--];
+		int rear = head + size;
+		// If we above the max index just take the difference which will wrap us around
+		rear = rear > maxIndex ? rear - maxIndex - 1 : rear;
+		size--;
+		return this.isEmpty() ? null : storage[rear];
 	};   
 
 	public E remove(E obj){
+		// iterator index gets set here
+		obj = find(obj);
+		if(obj == null){
+			return null;
+		}
+		else{
+			storage = shiftOver(storage,iteratorIndex);
+		}
 		return obj;
 	};
 
@@ -68,12 +91,18 @@ public class ArrayLinearList<E> implements LinearListADT<E> {
 	};
 
 	public boolean contains(E obj){
-		return true;
+		return find(obj) != null;
 	};  
 
 	public E find(E obj){
-		this.iterator();
-		return obj;
+		Iterator storageIterator = iterator();
+		while(storageIterator.hasNext()){
+			E current = (E)(storageIterator.next());
+			if( ((Comparable<E>)obj).compareTo(current) == 0){
+				return current;
+			};
+		};
+		return null;
 	};       
 
 	public void clear(){
@@ -97,7 +126,8 @@ public class ArrayLinearList<E> implements LinearListADT<E> {
 	public E[] shiftOver(E[] localArray,int index){
 		/* We want to shift the least number of elements as possible so we just shift towards
 		whatever side is closer i.g: if index being removed is in the lower half of the array shift the bottom
-		up.*/
+		up. Alternatively we can do a shift from the index but we would still have to see which direction
+		we move in in order to pick the best side. */
 		if ( (size >> 1) > index ){
 			for(int e = head; e < index; e++){
 				int wrappedE = e > maxIndex ? e - maxIndex : e;
@@ -123,34 +153,41 @@ public class ArrayLinearList<E> implements LinearListADT<E> {
 
 	// this needs to get finished
 	public Iterator<E> iterator(){
+		// Returning an anon class of Iterator
 		return new Iterator<E>(){
 			// We don't want to touch the the ArrayLinearList instance variables
-			int index = head;
-			E[] storage = this.storage;
+			int index = head - 1;
+			int count = -1;
+			E[] iteratorStorage = storage;
 
 			public boolean hasNext(){
+				if(size == -1){
+					return false;
+				};
 				int nextIndex = index + 1;
 				// Making sure index wraps
-				nextIndex = nextIndex > maxIndex ? nextIndex - maxIndex : nextIndex;
-				return nextIndex <= size && nextIndex != head;
+				nextIndex = nextIndex > maxIndex ? nextIndex - maxIndex - 1: nextIndex;
+				return count != size;
 			};
 			/* Tries to return next, regardless if there is a value there*/
 			public E next(){
 				if(!this.hasNext()){
-					throw new NullPointerException("No more values.");
+					throw new NoSuchElementException();
 				};
-				int nextIndex = index++;
-				nextIndex = nextIndex > maxIndex ? nextIndex - maxIndex : nextIndex;
-				return storage[nextIndex];
+				int nextIndex = ++index;
+				nextIndex = nextIndex > maxIndex ? nextIndex - maxIndex - 1: nextIndex;
+				iteratorIndex = nextIndex;
+				count++;
+				return iteratorStorage[nextIndex];
 			};
 
 			public void remove(){
 				// Shift basically handles the removal, we just need to pass it our local array and the index
-				this.storage = shiftOver(storage,index);
+				iteratorStorage = shiftOver(storage,index);
+				storage = iteratorStorage;
 			};
 		};
-	};    
-
+	};
 }
 
 
--- data_structures/driver.java
+++ data_structures/driver.java
@@ -1,86 +1,74 @@
 package data_structures;
+import java.util.Iterator;
 
 public class driver {
     public static void main(String[] args) {
-	    ArrayLinearList list = new ArrayLinearList(10);
+	    ArrayLinearList<Integer> list = new ArrayLinearList<Integer>(10);
+        System.out.println("added stuff");
+	    for(int i=1; i <= 10; i++) 
+            list.addFirst(i);
+        System.out.println("Should now print 10 .. 1");
+        for(Integer i : list)
+            System.out.println(i);
+        System.out.println("Finished Printing 10 - 1");
+        for(int i=1; i <= 10; i++) 
+            if(list.removeFirst() == null)
+                throw new RuntimeException("ERROR with removeFirst");
+        System.out.println("finished removing stuff");
+        for(Integer i : list)
+            System.out.println(i);
 
-	    System.out.println("list is empty.");
-	    System.out.println("(true) List should be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull());
+        System.out.println("REMOVAL OKAY");
+            
+        if(!list.isEmpty())
+            throw new RuntimeException("ERROR in inEmpty");  
+        if(list.size() != 0)
+            throw new RuntimeException("ERROR in size");
+            
+        System.out.println("Finished size checking");
+        for(int i=1; i <= 1000; i++) {
+            for(int j=1; j <= 7; j++)
+                list.addFirst(j);
+            for(int j=1; j <= 7; j++)                
+                list.removeLast();
+            }
 
-	    System.out.println("Add Last");
-	    list.addLast("uno");
-	    System.out.println("(false) List should not be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull()); 
+            System.out.println("Finished adding and removing");
+            
+        for(int i=1; i <= 1000; i++) {
+            for(int j=1; j <= 7; j++)
+                list.addLast(j);
+            for(int j=1; j <= 7; j++)                
+                list.removeFirst();
+            }
+            System.out.println("Finished Adding and Removing seoncd time");
+        list.addFirst(-1);
+        System.out.println("negative one worked");
+        if(list.peekLast() != -1)   
+            throw new RuntimeException("ERROR in peekLast");
+        list.clear();
+        list.addLast(-1);
+        if(list.peekFirst() != -1)   
+            throw new RuntimeException("ERROR in peekLast"); 
 
-	    System.out.println("Add First");
-	    list.addFirst("dos");
-	    System.out.println("(false) List should not be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull()); 
+        System.out.println("Peeking good.");
+            
+        list.clear();
+        for(int i=1; i <= 10; i++)
+            list.addFirst(i);
+        System.out.println("Add First works");
+        list.printArray();
 
-	    System.out.println("Clearing List.");
-	    list.clear();
-	    System.out.println("(true) List should be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull()); 
-
-	    System.out.println("Add First");
-	    list.addFirst("uno");
-	    System.out.println("(false) List should not be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull()); 
-
-	    System.out.println("Add Last");
-	    list.addLast("dos");
-	    System.out.println("(false) List should not be empty: " + list.isEmpty()); 
-	    System.out.println("(false) List should not be full: " + list.isFull()); 
-
-	    System.out.println("Peek First");
-	    System.out.println("(Uno) Should be Uno: " + list.peekFirst());
-	    System.out.println("Peek First");
-	    System.out.println("(Dos) Should be Dos: " + list.peekLast());
-
-	    System.out.println("Adding First.");
-	    list.addFirst("tres");
-	    System.out.println("(Tres) Should be tres: " + list.peekFirst());
-	    System.out.println("(Dos) Should be Dos: " + list.peekLast());
-
-  	    System.out.println("Adding Last.");
-	    list.addLast("cuatro");
-	    System.out.println("(Tres) Should be tres: " + list.peekFirst());
-	    System.out.println("(Cuatro) Should be Cuatro: " + list.peekLast());
+        for(int i=1; i <= 10; i++){
+            if(list.find(i) != i)
+                throw new RuntimeException("ERROR in find"); 
+        }
+        System.out.println("Find works");
+        for(int i=1; i <= 10; i++)
+            if(!list.contains(i))
+                throw new RuntimeException("ERROR in find"); 
+        System.out.println("Contains works");
 
 
-	    System.out.println("------ REMOVAL --------");
-	    list.printArray();
-	    System.out.println("Removing First.");
-	    list.removeFirst();
-	    System.out.println("(uno) Should be Uno: " + list.peekFirst());
-	   	System.out.println("(cuatro) Should be Cuatro: " + list.peekLast());
-	   	System.out.println("Removing Last.");
-	   	list.removeLast();
-	    System.out.println("(uno) Should be Uno: " + list.peekFirst());
-	   	System.out.println("(dos) Should be Dos: " + list.peekLast());
-
-  	    System.out.println("Adding First.");
-	    list.addFirst("44");
-	    System.out.println("(44) Should be 44: " + list.peekFirst());
-	    System.out.println("(Dos) Should be Dos: " + list.peekLast());
-
-  	    System.out.println("Adding Last.");
-	    list.addLast("1337");
-	    System.out.println("(44) Should be 44: " + list.peekFirst());
-	    System.out.println("(1337) Should be 1337: " + list.peekLast());
-
-	    System.out.println("(4) Size should be 4: " + list.size());
-	    list.printArray();
-
-	    System.out.println("Removing Last.");
-	    list.removeLast();
-	    System.out.println("(44) Should be 44: " + list.peekFirst());
-	    System.out.println("(dos) Should be Dos: " + list.peekLast());
-	    System.out.println("Removing Last Again.");
-	    list.removeLast();
-	    System.out.println("(44) Should be 44: " + list.peekFirst());
-	    System.out.println("(uno) Should be Uno: " + list.peekLast());
-	  
     }
 }
