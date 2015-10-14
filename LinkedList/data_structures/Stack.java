59c0465063449e7980f46db5206e542773a1f0b9
MichaelScript <me@michaelgreen.net>
2015-10-14 12:43:41 -0700

    Added finished LinkedList and the start of our third project

---
+++ LinkedList/data_structures/Stack.java
@@ -0,0 +1,65 @@
+/* Michael Green
+   masc0338
+ * A class assignment for CS310, a linked list implementation using an interface provided to us called
+ * ArrayLinearList
+ * @author Michael Green
+ */
+
+package data_structures;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Stack<E> implements Iterable<E>{
+
+	private LinearList<E> list = new LinearList<E>();
+
+      // inserts the object obj into the stack
+    public void push(E obj){
+    	list.addFirst(obj);
+    };
+    
+    // pops and returns the element on the top of the stack    
+    public E pop(){
+    	return list.removeFirst();
+    }
+    
+    // returns the number of elements currently in the stack
+    public int size(){
+    	return list.size();
+    }
+    
+    // return true if the stack is empty, otherwise false
+    public boolean isEmpty(){
+    	return list.isEmpty();
+    }
+    
+    // returns but does not remove the element on the top of the stack    
+    public E peek(){
+    	return list.peekFirst();
+    };
+     
+    // returns true if the object obj is in the stack,
+    // otherwise false   
+    public boolean contains(E obj){
+    	return list.contains(obj);
+    };
+    
+    // returns the stack to an empty state    
+    public void makeEmpty(){
+    	list.clear();
+    } 
+    
+    // removes the Object obj if it is in the stack and
+    // returns true, otherwise returns false.
+    public boolean remove(E obj){
+    	return list.remove(obj) == null ? false : true;
+    }
+    
+    // returns a iterator of the elements in the stack.  The elements
+    // must be in the same sequence as pop() would return them.    
+    public Iterator<E> iterator(){
+    	return list.iterator();
+    } 
+
+}
---
+++ LinkedList/data_structures/driver.java
@@ -0,0 +1,186 @@
+package data_structures;
+
+
+public class driver {
+    public static void main(String args[]){
+  //   	LinearListADT<Integer> list = new LinearList<Integer>();
+  //   	for(int x = 0; x < 20; x++){
+  //   		for(int i = 0; i < 999; i++){
+		//     	System.out.println("Attempting Operations on Empty Structure");
+		//     	System.out.println(list.find(6));
+		//     	System.out.println(list.contains(6));
+		//     	System.out.println(list.removeFirst());
+		//     	System.out.println(list.removeLast());
+		//     	System.out.println(list.size());
+		//     	System.out.println(list.isEmpty());
+		//     	System.out.println(list.isFull());
+	 //    	}
+
+	 //    	for(int i = 0; i < 250; i++){
+	 //    	System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Adding First. \n Current Size is: " + list.size());
+		//     System.out.println(list.addFirst(1));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//    	System.out.println("Adding First. \n Current Size is: " + list.size());
+		//     System.out.println(list.addFirst(2));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//     System.out.println("Adding Last. \n Current Size is: " + list.size());
+		//     System.out.println(list.addLast(3));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//      System.out.println("Adding Last. \n Current Size is: " + list.size());
+		//     System.out.println(list.addLast(4));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//      System.out.println("Adding Last. \n Current Size is: " + list.size());
+		//     System.out.println(list.addLast(8575));
+		// };
+		// for(int i = 0; i < 5000; i++){
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//     System.out.println("Adding First. \n Current Size is: " + list.size());
+		//     System.out.println(list.addFirst(6));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//     System.out.println("Adding First. \n Current Size is: " + list.size());
+		//     System.out.println(list.addFirst(7));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//      System.out.println("Adding Last. \n Current Size is: " + list.size());
+		//     System.out.println(list.addLast(8));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+		//      System.out.println("Adding Last. \n Current Size is: " + list.size());
+		//     System.out.println(list.addLast(9));
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+		//     System.out.println("Current Size now is: " + list.size() + "\n");
+
+
+		//      System.out.println("\nTesting Iteration: (Should print a bunch of nums)");
+		//     for(Integer j : list){
+		//     	System.out.println(j);
+		//     };
+
+		//     for(int j = 0; j < 5; j++){
+		//     	list.addLast(1337);
+		//     }
+		// };
+		// for(int i = 0; i < 7000; i++){
+		//     System.out.println("Should print then 1337 5 times.");
+		//     for(Integer j : list){
+		//     	System.out.println(j);
+		//     }
+
+		// 	System.out.println("Trying to find 3...");
+		//     System.out.println("Found: " + list.find(3));
+
+		//     System.out.println("Contains: " + list.contains(3));
+
+		//     System.out.println("\n\n--------");
+
+		//     System.out.println("Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeFirst());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println(" (last) Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeLast());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println(" (last) Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeLast());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println(" (last) Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeLast());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println("Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeFirst());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println("Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeFirst());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println("Testing Removal: Current Size is: " + list.size());
+		//     System.out.println(list.removeFirst());
+		//     System.out.println("Now Size is: " + list.size());
+
+		//     System.out.println("\n Testing Iteration: ");
+		//     for(Integer j : list){
+		//     	System.out.println(j);
+		//     };
+		// };
+		//    for(int i = 0; i < Math.random() * 100; i++){
+		//     System.out.println("Peek First is: " + list.peekFirst() + " Peek Last is: " + list.peekLast());
+
+		//     System.out.println("Trying to find 3...");
+		//     System.out.println("Found: " + list.find(3));
+		//     System.out.println("Contains: " + list.contains(3));
+
+		//     System.out.println("Adding Last: " + list.addLast(6));
+		//     System.out.println("Adding Last: " + list.addLast(8575));
+		//     System.out.println("Adding Last: " + list.addLast(2324));
+		//     System.out.println("Adding First: " + list.addFirst(616));
+		//     System.out.println("First is: " + list.peekFirst());
+		//     System.out.println("Trying to remove the number 616: " + list.remove(616));
+		//     System.out.println("Trying to remove 8575: " + list.remove(8575));
+
+		// };
+
+
+		//     if(x % 5 == 0){
+		//     	System.out.println("clearing");
+		//     	list.clear();
+		//     }
+		// };
+
+
+    	System.out.println("\nTesting Queue\n");
+    	Queue<Integer> queue = new Queue<Integer>();
+    	System.out.println("Adding to the queue: ");
+    	System.out.println("Queue Size is: " + queue.size());
+    	queue.enqueue(1);
+    	queue.enqueue(2);
+    	queue.enqueue(3);
+    	queue.dequeue();
+    	queue.dequeue();
+    	queue.enqueue(4);
+    	queue.enqueue(5);
+    	queue.dequeue();
+    	System.out.println("Queue Size is now: " + queue.size());
+    	for(Integer i : queue){
+	    	System.out.println(i);
+	    }
+	    System.out.println(queue.contains(5));
+	    System.out.println(queue.contains(2));
+	    System.out.println(queue.remove(5));
+	    System.out.println(queue.contains(5));
+	    System.out.println(queue.peek());
+	    System.out.println(queue.isEmpty());
+
+	    System.out.println("STACK STUFF");
+	    Stack<Integer> stack = new Stack<Integer>();
+	    System.out.println(stack.pop());
+	    stack.push(1);
+	    stack.push(2);
+	    stack.push(3);
+	    System.out.println(stack.pop());
+	    System.out.println(stack.pop());
+	    System.out.println(stack.pop());
+	    stack.push(6);
+	    stack.push(7);
+	    stack.push(8);
+	    System.out.println(stack.contains(6));
+	    System.out.println(stack.contains(7));
+	    System.out.println(stack.contains(8));
+	    System.out.println(stack.contains(9));
+
+
+
+
+
+    }
+}
